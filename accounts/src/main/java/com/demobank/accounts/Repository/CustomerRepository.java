66e0f50639d883e54ad5db76a70e1b4a865a87ac
Ojus M S <ojusms@gmail.com>
2026-08-13 19:10:30 -0700

    Update CustomerRepository with a method to find customer by mobile number

--- accounts/src/main/java/com/demobank/accounts/Repository/CustomerRepository.java
+++ accounts/src/main/java/com/demobank/accounts/Repository/CustomerRepository.java
@@ -4,6 +4,11 @@ import com.demobank.accounts.Entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    // declare an abstract method to find a customer by mobile number. Ensuring the method name follows proper convention.
+    // JpaRepository of Spring Data JPA takes care of the rest
+    Optional<Customer> findByMobileNumber(String mobileNumber);
 }
