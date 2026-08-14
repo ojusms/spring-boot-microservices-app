85ec1cb438b0cc8479e9fe3db394d145dd0e5594
Ojus M S <ojusms@gmail.com>
2026-08-13 19:02:21 -0700

    Add Exception package, CustomerAlreadyExistsException

---
+++ accounts/src/main/java/com/demobank/accounts/Package/CustomerAlreadyExistsException.java
@@ -0,0 +1,23 @@
+package com.demobank.accounts.Package;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Custom exception for when a a new customer is created with existing customer details.
+ */
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class CustomerAlreadyExistsException extends RuntimeException {
+
+    /**
+     * Constructs a new runtime exception with the specified detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public CustomerAlreadyExistsException(String message) {
+        super(message);
+    }
+}
