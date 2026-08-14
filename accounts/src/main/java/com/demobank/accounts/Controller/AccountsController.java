d2b2c26459bbeabc66e098f852f8c7effd990e33
Ojus M S <ojusms@gmail.com>
2026-08-13 19:53:17 -0700

    Updates to AccountsController class to save the Customer and Account for a new customer

--- accounts/src/main/java/com/demobank/accounts/Controller/AccountsController.java
+++ accounts/src/main/java/com/demobank/accounts/Controller/AccountsController.java
@@ -3,6 +3,8 @@ package com.demobank.accounts.Controller;
 import com.demobank.accounts.Constants.AccountsConstants;
 import com.demobank.accounts.DTO.CustomerDTO;
 import com.demobank.accounts.DTO.ResponseDTO;
+import com.demobank.accounts.Service.IAccountsService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,13 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@AllArgsConstructor
 public class AccountsController {
+
+    /* add AccountsService field for saving the Accounts object. Add Lombok annotation for all args
+     constructor to class so Spring can do the autowiring since there is only 1 constructor
+     */
+    IAccountsService iAccountsService;
 
     // POST mapping available at "/api/create".
     // The data passed from HTTP request is bound to the method parameter of type CustomerDTO
     @PostMapping("/create")
     public ResponseEntity<ResponseDTO> createAccount(@RequestBody CustomerDTO customerDTO) {
-
+        // save the account using the new Service object
+        iAccountsService.createAccount(customerDTO);
         // returning ResponseEntity instead of ResponseDTO directly because it allows us to
         // add some metadata such as HTTP status and header info, whereas in ResponseDTO, the response would
         // directly be only in the response body
