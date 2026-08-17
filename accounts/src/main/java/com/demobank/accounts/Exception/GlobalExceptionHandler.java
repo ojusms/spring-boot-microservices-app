971e956671ef42545c5acad4e13c39ed2c8e5329
Ojus M S <ojusms@gmail.com>
2026-08-16 18:25:28 -0700

    Update GlobalExceptionHandler with a method to handle input arguments being invalid

--- accounts/src/main/java/com/demobank/accounts/Exception/GlobalExceptionHandler.java
+++ accounts/src/main/java/com/demobank/accounts/Exception/GlobalExceptionHandler.java
@@ -1,20 +1,30 @@
 package com.demobank.accounts.Exception;
 
 import com.demobank.accounts.DTO.ErrorResponseDTO;
+import org.jspecify.annotations.Nullable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /* global exception handler class. It tells Spring to run
 the methods of this class when a Controller class throws an exception
  */
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     // tells Spring to run this method when CustomerAlreadyExistsException is thrown
     @ExceptionHandler(CustomerAlreadyExistsException.class)
@@ -56,5 +66,26 @@ public class GlobalExceptionHandler {
                 LocalDateTime.now()
         );
         return new ResponseEntity<>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    /*
+    After adding input validation to DTO classes, if an invalid input is sent to the API, it returns an error
+    with a detailed message similar to a stack trace. To better handle this, the below method of
+    ResponseEntityExceptionHandler class is overridden and used. Now the response to
+    client when an invalid input is sent is a simple JSON with the message for each invalid field
+     */
+    @Override
+    protected @Nullable ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String,String> validationErrors = new HashMap<>();
+        List<ObjectError> validationErrorsList = ex.getBindingResult().getAllErrors();
+
+        validationErrorsList.forEach((error) -> {
+            String fieldName = ((FieldError)error).getField();
+            String validationMsg = error.getDefaultMessage();
+            validationErrors.put(fieldName,validationMsg);
+        });
+
+        return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST);
     }
 }
