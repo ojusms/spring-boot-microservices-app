9084ee2e0cdc40e323efdb721cedf7ef4f3da7d2
Ojus M S <ojusms@gmail.com>
2026-08-17 18:23:08 -0700

    Updates to AccountsController to enhance Swagger UI OpenAPI documentation for APIs

--- accounts/src/main/java/com/demobank/accounts/Controller/AccountsController.java
+++ accounts/src/main/java/com/demobank/accounts/Controller/AccountsController.java
@@ -4,6 +4,10 @@ import com.demobank.accounts.Constants.AccountsConstants;
 import com.demobank.accounts.DTO.CustomerDTO;
 import com.demobank.accounts.DTO.ResponseDTO;
 import com.demobank.accounts.Service.IAccountsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -17,6 +21,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
 @Validated
+@Tag(name = "REST APIs for Accounts service of DemoBank", // update swagger api docs
+        description = "REST API docs of CREATE, READ, UPDATE, and DELETE operations for Accounts service of DemoBank")
 public class AccountsController {
 
     /* add AccountsService field for saving the Accounts object. Add Lombok annotation for all args
@@ -26,6 +32,11 @@ public class AccountsController {
 
     // POST mapping available at "/api/create".
     // The data passed from HTTP request is bound to the method parameter of type CustomerDTO
+    @Operation(
+            summary = "CREATE REST API", // update individual api doc in swagger ui
+    description = "REST API to create a Customer and Account in DemoBank")
+    @ApiResponse(description = "HTTP Status CREATED", // update response schema in swagger api doc ui
+    responseCode = "201")
     @PostMapping("/create")
     public ResponseEntity<ResponseDTO> createAccount(@Valid @RequestBody CustomerDTO customerDTO) {
         // save the account using the new Service object
@@ -40,6 +51,14 @@ public class AccountsController {
 
     // GET mapping at "/api/fetch" to find and return a customer's details by mobile number
     // mobileNumber method argument is mapped to the request query parameter in the url
+    @Operation(
+            summary = "READ REST API",
+            description = "REST API to read a customer and account of DemoBank"
+    )
+    @ApiResponse(
+            description = "HTTP Status OK",
+            responseCode = "200"
+    )
     @GetMapping("/fetch")
     public ResponseEntity<CustomerDTO> fetchAccountDetails(
             @RequestParam
@@ -53,6 +72,20 @@ public class AccountsController {
     PUT mapping at "/api/update" to accept a customerDTO format in the body and update an existing customer
     and related account details.
      */
+    @Operation(
+            summary = "UPDATE REST API",
+            description = "REST API to update a customer and/or account of DemoBank"
+    )
+    @ApiResponses({ // use this tag for multiple possible responses in the swagger api doc ui
+            @ApiResponse(
+                    description = "HTTP Status OK",
+                    responseCode = "200"
+            ),
+            @ApiResponse(
+                    description = "HTTP Status INTERNAL_SERVER_ERROR",
+                    responseCode = "500"
+            )
+    })
     @PutMapping("/update")
     public ResponseEntity<ResponseDTO> updateAccountDetails(@Valid @RequestBody CustomerDTO customerDTO) {
         boolean updated = iAccountsService.updateAccount(customerDTO);
@@ -72,6 +105,20 @@ public class AccountsController {
     DELETE mapping at "/api/delete" to delete a customer and account by accepting
     mobile number in query parameter
     */
+    @Operation(
+            summary = "DELETE REST API",
+            description = "Rest API to delete a customer and account of DemoBank"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    description = "HTTP Status OK",
+                    responseCode = "200"
+            ),
+            @ApiResponse(
+                    description = "HTTP Status INTERNAL_SERVER_EXCEPTION",
+                    responseCode = "500"
+            )
+    })
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDTO> deleteAccount(
             @RequestParam
