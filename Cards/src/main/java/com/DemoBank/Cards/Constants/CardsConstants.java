5ba654649c8bd5b26f3600212f5f408b9de340a1
Ojus M S <ojusms@gmail.com>
2026-08-19 20:05:26 -0700

    Cards: Add Constants package with CardsConstants class

---
+++ Cards/src/main/java/com/DemoBank/Cards/Constants/CardsConstants.java
@@ -0,0 +1,20 @@
+package com.DemoBank.Cards.Constants;
+
+public final class CardsConstants {
+
+    private CardsConstants() {
+    } // restrict instantiation
+
+    private static final String CREDIT_CARD = "Credit Card";
+    private static final int NEW_CARD_LIMIT = 100000;
+    private static final String STATUS_201 = "201";
+    private static final String MESSAGE_201 = "Card created successfully";
+    private static final String STATUS_200 = "200";
+    private static final String MESSAGE_200 = "Request processed successfully";
+    private static final String STATUS_500 = "500";
+    private static final String MESSAGE_500 = "An error occurred. Please try again.";
+    private static final String STATUS_417 = "417";
+    private static final String MESSAGE_417_UPDATE = "Update operation failed. Please try again";
+    private static final String MESSAGE_417_DELETE = "Delete operation failed. Please try again";
+
+}
