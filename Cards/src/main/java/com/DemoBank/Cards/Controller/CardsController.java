b362b7ee54ad96532ade4df4191b6afc7ac4e22d
Ojus M S <ojusms@gmail.com>
2026-08-19 21:36:32 -0700

    Cards: Update CardsController with input validation

--- Cards/src/main/java/com/DemoBank/Cards/Controller/CardsController.java
+++ Cards/src/main/java/com/DemoBank/Cards/Controller/CardsController.java
@@ -3,10 +3,13 @@ package com.DemoBank.Cards.Controller;
 import com.DemoBank.Cards.Constants.CardsConstants;
 import com.DemoBank.Cards.DTO.ResponseDTO;
 import com.DemoBank.Cards.Service.ICardsService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,12 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
+@Validated
 public class CardsController {
 
     private ICardsService iCardsService;
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseDTO> createCard(@RequestParam String mobileNumber) {
+    public ResponseEntity<ResponseDTO> createCard(
+            @Valid
+            @Pattern(regexp = "^$|[0-9]{10}")
+            @RequestParam String mobileNumber) {
         iCardsService.createCard(mobileNumber);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
