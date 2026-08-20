64259fbd68a5df91e428b2fe4e756baef1f0887b
Ojus M S <ojusms@gmail.com>
2026-08-19 18:21:36 -0700

    Cards: Add Cards Entity class to Entity package

---
+++ Cards/src/main/java/com/DemoBank/Cards/Entity/Cards.java
@@ -0,0 +1,42 @@
+package com.DemoBank.Cards.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
+public class Cards extends BaseEntity {
+
+    @Id
+    @Column(name = "card_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int cardId;
+
+    @Column(name = "mobile_number")
+    private Long mobileNumber;
+
+    @Column(name = "card_number")
+    private Long cardNumber;
+
+    @Column(name = "card_type")
+    private String cardType;
+
+    @Column(name = "total_limit")
+    private int totalLimit;
+
+    @Column(name = "amount_used")
+    private int amountUsed;
+
+    @Column(name = "available_amount")
+    private int availableAmount;
+
+
+}
