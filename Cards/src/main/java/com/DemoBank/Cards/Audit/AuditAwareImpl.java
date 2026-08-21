cac25b119391b3fcaaf1566d8f968c7ec98a61e5
Ojus M S <ojusms@gmail.com>
2026-08-20 23:18:01 -0700

    Cards: Updates to enable Auditing by Spring and populate Created and Updated metadata fields dynamically

--- Cards/src/main/java/com/DemoBank/Cards/Audit/AuditAwareImpl.java
+++ Cards/src/main/java/com/DemoBank/Cards/Audit/AuditAwareImpl.java
@@ -1,9 +1,11 @@
 package com.DemoBank.Cards.Audit;
 
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+@Component("auditAwareImpl")
 public class AuditAwareImpl implements AuditorAware<String> {
     /**
      * Returns the current auditor of the application.
--- Cards/src/main/java/com/DemoBank/Cards/Entity/BaseEntity.java
+++ Cards/src/main/java/com/DemoBank/Cards/Entity/BaseEntity.java
@@ -1,6 +1,7 @@
 package com.DemoBank.Cards.Entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,11 +10,13 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter @Setter @ToString
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
     @Column(updatable = false)
