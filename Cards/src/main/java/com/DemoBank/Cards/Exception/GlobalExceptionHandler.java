3fba6657f0548a9c005c1d09483637383a7ad440
Ojus M S <ojusms@gmail.com>
2026-08-20 20:04:01 -0700

    Cards: Update GlobalExceptionHandler with a method to handle input arguments being invalid

--- Cards/src/main/java/com/DemoBank/Cards/Exception/GlobalExceptionHandler.java
+++ Cards/src/main/java/com/DemoBank/Cards/Exception/GlobalExceptionHandler.java
@@ -1,14 +1,23 @@
 package com.DemoBank.Cards.Exception;
 
 import com.DemoBank.Cards.DTO.ErrorResponseDTO;
+import org.jspecify.annotations.Nullable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -23,5 +32,18 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 LocalDateTime.now()
         );
         return new ResponseEntity<>(errorResponseDTO, HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected @Nullable ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String,String> validationErrors = new HashMap<>();
+        List<ObjectError> objectErrorList = ex.getBindingResult().getAllErrors();
+        objectErrorList.forEach((error -> {
+            String fieldName = ((FieldError)error).getField();
+            String validationMsg = error.getDefaultMessage();
+            validationErrors.put(fieldName,validationMsg);
+        }));
+        return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST);
     }
 }
