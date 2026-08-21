53ca94787f05f9eecc207648eef70c45d1123aa6
Ojus M S <ojusms@gmail.com>
2026-08-20 20:23:55 -0700

    Cards: Update CardsController with an endpoint and method stub to find card details

--- Cards/src/main/java/com/DemoBank/Cards/Controller/CardsController.java
+++ Cards/src/main/java/com/DemoBank/Cards/Controller/CardsController.java
@@ -1,6 +1,7 @@
 package com.DemoBank.Cards.Controller;
 
 import com.DemoBank.Cards.Constants.CardsConstants;
+import com.DemoBank.Cards.DTO.CardsDTO;
 import com.DemoBank.Cards.DTO.ResponseDTO;
 import com.DemoBank.Cards.Service.ICardsService;
 import jakarta.validation.Valid;
@@ -10,10 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -26,11 +24,19 @@ public class CardsController {
     @PostMapping("/create")
     public ResponseEntity<ResponseDTO> createCard(
             @Valid
-            @Pattern(regexp = "^$|[0-9]{10}", message = "mobile number must be 10 digits")
+            @Pattern(regexp = "^$|[0-9]{10}", message = "Mobile number must be 10 digits")
             @RequestParam String mobileNumber) {
         iCardsService.createCard(mobileNumber);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(new ResponseDTO(CardsConstants.STATUS_201, CardsConstants.MESSAGE_201));
+    }
+
+    @GetMapping("/fetch")
+    public ResponseEntity<CardsDTO> findCard(
+            @Valid
+            @Pattern(regexp = "^$|[0-9]{10}",message = "Mobile number must be 10 digits")
+            @RequestParam String mobileNumber) {
+        return null;
     }
 }
