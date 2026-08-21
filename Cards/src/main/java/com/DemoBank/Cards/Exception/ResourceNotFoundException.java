b6fa451e68f655d373c71adb95be9506ce7b898c
Ojus M S <ojusms@gmail.com>
2026-08-20 20:52:51 -0700

    Cards: Update Exception package with new ResourceNotFoundException

---
+++ Cards/src/main/java/com/DemoBank/Cards/Exception/ResourceNotFoundException.java
@@ -0,0 +1,17 @@
+package com.DemoBank.Cards.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    /**
+     *
+     * @param resourceName the resource that was looked up
+     * @param fieldName the parameter that was queried
+     * @param fieldValue the value that was input for the query parameter
+     */
+    public ResourceNotFoundException(String resourceName, String fieldName, String fieldValue) {
+                super(String.format("'%s not found for given input data %s : %s'",resourceName,fieldName,fieldValue));
+    }
+}
