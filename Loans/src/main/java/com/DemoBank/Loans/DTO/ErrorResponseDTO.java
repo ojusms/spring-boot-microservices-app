3ddcbc2a44a5fa5e94c3a444a70e5415213bf95b
Ojus M S <ojusms@gmail.com>
2026-08-23 19:22:50 -0700

    Loans: Update DTO package with ResponseDTO and ErrorResponseDTO classes

---
+++ Loans/src/main/java/com/DemoBank/Loans/DTO/ErrorResponseDTO.java
@@ -0,0 +1,19 @@
+package com.DemoBank.Loans.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Data @AllArgsConstructor
+public class ErrorResponseDTO {
+
+    private String apiPath;
+
+    private HttpStatus errorCode;
+
+    private String errorMessage;
+
+    private LocalDateTime errorDate;
+}
