9d1e31cfce2640a003903b1a961ba4518fcdd2cc
Ojus M S <ojusms@gmail.com>
2026-08-23 18:17:24 -0700

    Cards: Updates to CardsController to enhance Swagger UI OpenAPI documentation for APIs

--- Cards/src/main/java/com/DemoBank/Cards/Controller/CardsController.java
+++ Cards/src/main/java/com/DemoBank/Cards/Controller/CardsController.java
@@ -2,8 +2,15 @@ package com.DemoBank.Cards.Controller;
 
 import com.DemoBank.Cards.Constants.CardsConstants;
 import com.DemoBank.Cards.DTO.CardsDTO;
+import com.DemoBank.Cards.DTO.ErrorResponseDTO;
 import com.DemoBank.Cards.DTO.ResponseDTO;
 import com.DemoBank.Cards.Service.ICardsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -17,10 +24,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
 @Validated
+@Tag(name = "REST APIs for Cards service of DemoBank",
+description = "REST API docs of CREATE, READ, UPDATE, and DELETE operations for Cards service of DemoBank")
 public class CardsController {
 
     private ICardsService iCardsService;
 
+    @Operation(summary = "CREATE REST API",
+            description = "REST API to create a new Card for a Customer in DemoBank")
+    @ApiResponse(description = "HTTP Status Created",
+            responseCode = "201")
     @PostMapping("/create")
     public ResponseEntity<ResponseDTO> createCard(
             @Valid
@@ -32,6 +45,14 @@ public class CardsController {
                 .body(new ResponseDTO(CardsConstants.STATUS_201, CardsConstants.MESSAGE_201));
     }
 
+    @Operation(
+            summary = "READ REST API",
+            description = "REST API to fetch the Card details of a customer of DemoBank"
+    )
+    @ApiResponse(
+            description = "HTTP Status OK",
+            responseCode = "200"
+    )
     @GetMapping("/fetch")
     public ResponseEntity<CardsDTO> findCard(
             @Valid
@@ -41,6 +62,27 @@ public class CardsController {
         return ResponseEntity.status(HttpStatus.OK).body(cardsDTO);
     }
 
+    @Operation(
+            summary = "UPDATE REST API",
+            description = "REST API to update a Card details of a customer of DemoBank"
+    )
+    @ApiResponses({ // use this tag for multiple possible responses in the swagger api doc ui
+            @ApiResponse(
+                    description = "HTTP Status OK",
+                    responseCode = "200"
+            ),
+            @ApiResponse(
+                    description = "HTTP Status EXPECTATION_FAILED",
+                    responseCode = "417",
+                    /* since error response dto is only sent from the
+                    exception handler, we have to give the reference like this so it shows up in
+                    the Swagger UI for API docs
+                     */
+                    content = @Content(
+                            schema = @Schema(implementation= ErrorResponseDTO.class)
+                    )
+            )
+    })
     @PutMapping("/update")
     public  ResponseEntity<ResponseDTO> updateCardDetails(@Valid @RequestBody CardsDTO cardsDTO) {
         boolean isUpdated = false;
@@ -57,6 +99,20 @@ public class CardsController {
         }
     }
 
+    @Operation(
+            summary = "DELETE REST API",
+            description = "Rest API to delete a Card of customer of DemoBank"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    description = "HTTP Status OK",
+                    responseCode = "200"
+            ),
+            @ApiResponse(
+                    description = "HTTP Status EXPECTATION_FAILED",
+                    responseCode = "417"
+            )
+    })
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDTO> deleteCard(
             @Valid
