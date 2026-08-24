e45f601039795c03d8de7a42eb1b25be883f3105
Ojus M S <ojusms@gmail.com>
2026-08-23 20:06:35 -0700

    Loans: Add Service package with LoansService interface and implementation with a method to create a loan

---
+++ Loans/src/main/java/com/DemoBank/Loans/Service/ILoansService.java
@@ -0,0 +1,9 @@
+package com.DemoBank.Loans.Service;
+
+public interface ILoansService {
+    /**
+     *
+     * @param mobileNumber input mobile number of customer
+     */
+    void createLoan(String mobileNumber);
+}
---
+++ Loans/src/main/java/com/DemoBank/Loans/Service/Impl/LoansServiceImpl.java
@@ -0,0 +1,48 @@
+package com.DemoBank.Loans.Service.Impl;
+
+import com.DemoBank.Loans.Constants.LoansConstants;
+import com.DemoBank.Loans.Entity.Loans;
+import com.DemoBank.Loans.Repository.LoansRepository;
+import com.DemoBank.Loans.Service.ILoansService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+@AllArgsConstructor
+public class LoansServiceImpl implements ILoansService {
+
+    private LoansRepository loansRepository;
+    /**
+     *
+     * @param mobileNumber input mobile number of customer
+     */
+    @Override
+    public void createLoan(String mobileNumber) {
+        Optional<Loans> loans = loansRepository.findByMobileNumber(mobileNumber);
+        if (loans.isPresent()) {
+            // TO DO: create new LoanAlreadyExistsException and throw it here
+            throw new RuntimeException("Loan already exists for given mobile number");
+        }
+        loansRepository.save(createNewLoan(mobileNumber));
+    }
+
+    /**
+     * helper method to create a new Loan for a given mobile number
+     * @param mobileNumber input mobile number of the customer
+     * @return Loans object with populated details
+     */
+    private Loans createNewLoan(String mobileNumber) {
+        Loans newLoan = new Loans();
+        newLoan.setMobileNumber(mobileNumber);
+        long randomLoanNumber = 100000000000L + new Random().nextInt(900000000);
+        newLoan.setLoanNumber(Long.toString(randomLoanNumber));
+        newLoan.setLoanType(LoansConstants.HOME_LOAN);
+        newLoan.setTotalLoan(LoansConstants.NEW_LOAN_LIMIT);
+        newLoan.setAmountPaid(0);
+        newLoan.setOutstandingAmount(LoansConstants.NEW_LOAN_LIMIT);
+        return newLoan;
+    }
+}
