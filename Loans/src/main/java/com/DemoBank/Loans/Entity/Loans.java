5e6afb80769ab674e574f21014204b62ab72d9d6
Ojus M S <ojusms@gmail.com>
2026-08-23 19:03:21 -0700

    Loans: Add Loans Entity class to Entity package

---
+++ Loans/src/main/java/com/DemoBank/Loans/Entity/Loans.java
@@ -0,0 +1,41 @@
+package com.DemoBank.Loans.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor @ToString
+public class Loans extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "loan_id")
+    private Integer loanId;
+
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+
+    @Column(name = "loan_number")
+    private String loanNumber;
+
+    @Column(name = "loan_type")
+    private String loanType;
+
+    @Column(name = "total_loan")
+    private int totalLoan;
+
+    @Column(name = "amount_paid")
+    private int amountPaid;
+
+    @Column(name = "outstanding_amount")
+    private int outstandingAmount;
+}
+
