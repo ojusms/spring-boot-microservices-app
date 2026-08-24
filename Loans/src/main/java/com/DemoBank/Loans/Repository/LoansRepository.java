95bc9ea20714590998acf7ab43e234bea4baf9df
Ojus M S <ojusms@gmail.com>
2026-08-23 19:55:30 -0700

    Loans: Update LoansRepository with a method to find a Loan by mobile number

--- Loans/src/main/java/com/DemoBank/Loans/Repository/LoansRepository.java
+++ Loans/src/main/java/com/DemoBank/Loans/Repository/LoansRepository.java
@@ -4,6 +4,10 @@ import com.DemoBank.Loans.Entity.Loans;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LoansRepository extends JpaRepository<Loans, Integer> {
+
+    Optional<Loans> findByMobileNumber(String mobileNumber);
 }
