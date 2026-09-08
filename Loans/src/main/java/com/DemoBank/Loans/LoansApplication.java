10425555ed22c618003662652c93090323e43982
Ojus M S <ojusms@gmail.com>
2026-09-08 15:09:42 -0700

    Loans: Update DTO package with LoansContactInfoDTO record, main app to enable use of ConfigurationProperty annotated beans

--- Loans/src/main/java/com/DemoBank/Loans/LoansApplication.java
+++ Loans/src/main/java/com/DemoBank/Loans/LoansApplication.java
@@ -1,5 +1,6 @@
 package com.DemoBank.Loans;
 
+import com.DemoBank.Loans.DTO.LoansContactInfoDTO;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -7,6 +8,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -31,6 +33,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 				url = "demobank.com"
 		)
 )
+@EnableConfigurationProperties(value = LoansContactInfoDTO.class)
 public class LoansApplication {
 
 	public static void main(String[] args) {
