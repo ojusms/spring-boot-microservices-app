ab5891f78104042b03b16b2d78d5bc31050f5165
Ojus M S <ojusms@gmail.com>
2026-09-10 14:36:34 -0700

    Loans: Change LoansContactInfoDTO to class from record to use Setter methods

--- Loans/src/main/java/com/DemoBank/Loans/DTO/LoansContactInfoDTO.java
+++ Loans/src/main/java/com/DemoBank/Loans/DTO/LoansContactInfoDTO.java
@@ -1,10 +1,16 @@
 package com.DemoBank.Loans.DTO;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "loans")
-public record LoansContactInfoDTO(String message, Map<String,String> contactDetails, List<String> onCallSupport) {
+@Getter @Setter
+public class LoansContactInfoDTO {
+    private String message;
+    private Map<String, String> contactInfo;
+    private List<String> onCallSupport;
 }
