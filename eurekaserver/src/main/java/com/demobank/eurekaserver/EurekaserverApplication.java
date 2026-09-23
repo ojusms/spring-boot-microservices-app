1adc65140f5ec9170829a965d763ff7599ed1213
Ojus M S <ojusms@gmail.com>
2026-09-22 20:06:13 -0700

    EurekaServer: Update main app to enable Eureka Server auto configuration

--- eurekaserver/src/main/java/com/demobank/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/demobank/eurekaserver/EurekaserverApplication.java
@@ -2,8 +2,17 @@ package com.demobank.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/*
+Eureka Server, part of Spring Cloud Netflix, is a Service Registry server that keeps track of microservices
+instances such as their IP, DNS, metadata, etc. Does Service Discovery by giving relevant info to microservice
+instances when they want to know how to contact other microservice instances for communication
+(like backing services or downstream dependencies) in client-side-service-discovery.
+ */
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaserverApplication {
 
 	public static void main(String[] args) {
