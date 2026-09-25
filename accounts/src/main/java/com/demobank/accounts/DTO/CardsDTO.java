8baa121d28166f11aa856e29c450b27914dae71c
Ojus M S <ojusms@gmail.com>
2026-09-24 20:07:42 -0700

    Accounts: Update DTO package with CardsDTO class

---
+++ accounts/src/main/java/com/demobank/accounts/DTO/CardsDTO.java
@@ -0,0 +1,69 @@
+package com.demobank.accounts.DTO;
+
+/*
+Having a local copy of CardsDTO file in Accounts service for use in FeignClient response after invoking
+Cards REST API. Having a copy instead of import from Cards (adding Cards as a dependency in Maven)
+to keep microservices independent and de-couple from build-time dependency. Allows independent releases of each service.
+ */
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+@Schema(
+        name = "Cards",
+        description = "Schema for Cards of DemoBank"
+)
+public class CardsDTO {
+
+    @NotEmpty(message = "Mobile number cannot be null or empty")
+    @Pattern(regexp = "^$|[0-9]{10}", message = "mobileNumber must be 10 digits")
+    @Schema(
+            description = "Mobile number of Customer of DemoBank",
+            example = "7896541230"
+    )
+    private String mobileNumber;
+
+    @NotEmpty(message = "Card number cannot be null or empty")
+    @Pattern(regexp = "^$|[0-9]{12}", message = "cardNumber must be 12 digits")
+    @Schema(
+            description = "Card Number of the Card of Customer of DemoBank",
+            example = "987456321753"
+    )
+    private String cardNumber;
+
+    @NotEmpty(message = "Card type cannot be null or empty")
+    @Schema(
+            description = "Type of Card",
+            example = "Credit Card"
+    )
+    private String cardType;
+
+    @Positive(message = "Total limit must be greater than 0")
+    @Schema(
+            description = "Total limit for the Card",
+            example = "100000"
+    )
+    private int totalLimit;
+
+    @PositiveOrZero(message = "Amount used must be greater than or equal to 0")
+    @Schema(
+            description = "Current amount used out of the total limit for the Card",
+            example = "25000"
+    )
+    private int amountUsed;
+
+    @PositiveOrZero(message = "Available amount must be greater than or equal to 0")
+    @Schema(
+            description = "Current amount available to use from the total limit",
+            example = "75000"
+    )
+    private int availableAmount;
+
+}
