57ffb7039ffa37db414c2650926bf3cf1f785dbe
Ojus M S <ojusms@gmail.com>
2026-09-24 21:11:27 -0700

    Accounts: Update fetchCustomerDetails() of CustomersService implementation with logic

--- accounts/src/main/java/com/demobank/accounts/Service/Impl/CustomersServiceImpl.java
+++ accounts/src/main/java/com/demobank/accounts/Service/Impl/CustomersServiceImpl.java
@@ -1,12 +1,21 @@
 package com.demobank.accounts.Service.Impl;
 
+import com.demobank.accounts.DTO.AccountsDTO;
+import com.demobank.accounts.DTO.CardsDTO;
 import com.demobank.accounts.DTO.CustomerDetailsDTO;
+import com.demobank.accounts.DTO.LoansDTO;
+import com.demobank.accounts.Entity.Accounts;
+import com.demobank.accounts.Entity.Customer;
+import com.demobank.accounts.Exception.ResourceNotFoundException;
+import com.demobank.accounts.Mapper.AccountsMapper;
+import com.demobank.accounts.Mapper.CustomerMapper;
 import com.demobank.accounts.Repository.AccountsRepository;
 import com.demobank.accounts.Repository.CustomerRepository;
 import com.demobank.accounts.Service.Client.CardsFeignClient;
 import com.demobank.accounts.Service.Client.LoansFeignClient;
 import com.demobank.accounts.Service.ICustomersService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -25,6 +34,24 @@ public class CustomersServiceImpl implements ICustomersService {
      */
     @Override
     public CustomerDetailsDTO fetchCustomerDetails(String mobileNumber) {
-        return null;
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Customer","mobileNumber",mobileNumber)
+        );
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
+                () -> new ResourceNotFoundException("Account","customerId", customer.getCustomerId().toString())
+        );
+        // populate Customer details into CustomerDetailsDTO
+        CustomerDetailsDTO customerDetailsDTO = CustomerMapper.mapToCustomerDetailsDTO(customer, new CustomerDetailsDTO());
+        // populate Accounts details into CustomerDetailsDTO
+        customerDetailsDTO.setAccountsDTO(AccountsMapper.mapToAccountsDTO(accounts, new AccountsDTO()));
+        // make an API call to Cards service via FeignClient to get Cards details
+        ResponseEntity<CardsDTO> cardsDTOResponseEntity = cardsFeignClient.findCard(mobileNumber);
+        // populate Cards details into CustomerDetailsDTO
+        customerDetailsDTO.setCardsDTO(cardsDTOResponseEntity.getBody());
+        // make an API call to Loans service via FeignClient to get Loans details
+        ResponseEntity<LoansDTO> loansDTOResponseEntity = loansFeignClient.findLoan(mobileNumber);
+        // populate Loans details into CustomerDetailsDTO
+        customerDetailsDTO.setLoansDTO(loansDTOResponseEntity.getBody());
+        return customerDetailsDTO;
     }
 }
